82e0e1715ee1cd06aacd62f19d1c61958281ca1b
andresruizmarin16 <andresruizmarin16@gmail.com>
2025-02-17 21:43:02 +0100

    subida de contador de palabras

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # programas_python
 Programas en pyhton
 1. Programa piedra, papel o tijeras
+2. contador de palabras
---
+++ contador_de_palabras.py
@@ -0,0 +1,5 @@
+print("Contador de palabras")
+frase = input("Ingresa una palabra: ").split(" ")
+palabras = len(frase)
+print(frase)
+print(palabras)
